af8949467fc44f9939a1fd67b010b1527955d07e
Ángela Vargas <a.vargasa@alumnos.urjc.es>
2019-06-12 15:04:15 +0200

    [angelavargas] Fixed download of certificates

--- app/pyploma.py
+++ app/pyploma.py
@@ -11,7 +11,7 @@ def generate(filename,level,language):
 
 
     base_dir = os.getcwd()
-    #os.chdir("/var/www/drScratch/app/certificate")
+    #os.chdir("/var/www/drscratchv3/app/certificate")
     os.chdir(base_dir + "/app/certificate")
     salida = open("output.tex","w") # crea fichero LaTeX para cada persona
     person = [filename,level] # pasar la cadena en lista ["testing.sb2","21"]
