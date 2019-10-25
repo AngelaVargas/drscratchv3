200ea88b1f5b0ec4f2cc547e9fe82b968c506f8e
cdchushig <cdavid.chushig@gmail.com>
2019-10-25 09:23:49 +0200

    [cdchushig] Fix bug in deadcode for opcode with argument_reporter

--- app/deadCode.py
+++ app/deadCode.py
@@ -1,6 +1,9 @@
-
 import json
 import zipfile
+
+import logging
+
+logger = logging.getLogger(__name__)
 
 
 class DeadCode():
@@ -10,8 +13,12 @@ class DeadCode():
     def __init__(self):
 
       self.dead_code_instances = 0
-      self.event_variables = ["event_broadcastandwait", "event_whenflagclicked", "event_whengreaterthan","event_whenkeypressed",
-                              "event_whenthisspriteclicked", "event_whenbackdropswitchesto", "procedures_prototype", "procedures_definition"]
+      self.opcode_argument_reporter = "argument_reporter"
+      self.event_variables = ["event_broadcastandwait", "event_whenflagclicked",
+                              "event_whengreaterthan", "event_whenkeypressed",
+                              "event_whenthisspriteclicked", "event_whenbackdropswitchesto",
+                              "procedures_prototype", "procedures_definition"]
+
       self.loop_blocks = ["control_repeat", "control_forever", "control_if", "control_if_else", "control_repeat_until"]
 
 
@@ -29,27 +36,29 @@ class DeadCode():
             blocks_list = []
             for _, blocks_dicc in dicc["blocks"].iteritems():
                if type(blocks_dicc) is dict:   
-                 event_variable = any(blocks_dicc["opcode"]== event for event in self.event_variables)
+                 event_variable = any(blocks_dicc["opcode"] == event for event in self.event_variables)
                  loop_block = any(blocks_dicc["opcode"] == loop for loop in self.loop_blocks)
 
                  if event_variable == False:
-                   if blocks_dicc["parent"] == None and blocks_dicc["next"] == None:
-                      blocks_list.append(str(blocks_dicc["opcode"]))
 
-                   #Check dead loop blocks
-                   if loop_block == True and blocks_dicc["opcode"] not in blocks_list:
-                      if not blocks_dicc["inputs"]:
-                         #Empty loop block, but inside of a block structure
-                         blocks_list.append(str(blocks_dicc["opcode"]))
-                      elif "SUBSTACK" not in blocks_dicc["inputs"]:
-                         blocks_list.append(str(blocks_dicc["opcode"]))
-                      else:
-                         #Could be normal loop block
-                         if blocks_dicc["inputs"]["SUBSTACK"][1] == None:
-                            blocks_list.append(str(blocks_dicc["opcode"]))
+                   if not self.opcode_argument_reporter in blocks_dicc["opcode"]:
 
-                
-            
+                       if blocks_dicc["parent"] == None and blocks_dicc["next"] == None:
+                          blocks_list.append(str(blocks_dicc["opcode"]))
+
+                       #Check dead loop blocks
+                       if loop_block and blocks_dicc["opcode"] not in blocks_list:
+                          if not blocks_dicc["inputs"]:
+                             #Empty loop block, but inside of a block structure
+                             blocks_list.append(str(blocks_dicc["opcode"]))
+                          elif "SUBSTACK" not in blocks_dicc["inputs"]:
+                             blocks_list.append(str(blocks_dicc["opcode"]))
+                          else:
+                             #Could be normal loop block
+                             if blocks_dicc["inputs"]["SUBSTACK"][1] == None:
+                                blocks_list.append(str(blocks_dicc["opcode"]))
+
+
             if blocks_list:
               sprites[sprite] = blocks_list
               self.dead_code_instances += 1
